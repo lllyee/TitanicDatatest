e9b8bfa3d3e8579bcd7f2a91bb4e77f85ddea362
yi.liu04 <yi.liu04@wormpex.com>
2018-07-03 11:41:53 +0800

    push

--- TitanicDatatest.py
+++ TitanicDatatest.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 pd.set_option('display.width', 1000)#不换行输出
 TData=pd.read_excel('/Users/yiliu/lllyeeData/titanic.xls')
 #TData.info()
@@ -6,3 +7,8 @@ TData=pd.read_excel('/Users/yiliu/lllyeeData/titanic.xls')
 print(TData.describe())
 SurvivalRate=round((100*TData['survived'].sum())/TData['survived'].count(),2)
 print('Titanic数据集共有{}条成员记录，有{}个成员生还，总体生还率为{}%'.format(TData['survived'].count(),TData['survived'].sum(),SurvivalRate))
+#TData.groupby('survived')['survived'].count().plot(kind='bar')#groupby 聚合运算  bar:条形图 barh:横向条形图
+#data.plot(kind='bar')
+TData.groupby('pclass')['name'].count().plot(kind='bar')
+#print(TData.groupby('pclass')['name'].count())
+plt.show()
